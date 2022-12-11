27681bba08282f87aca34fb844be5f4d6c14518a
ayush-corp <ayush.fullerton@gmail.com>
2022-12-10 17:56:31 -0800

    server commit

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import json
+import time
 from quart import Quart, request
 
 app = Quart(__name__)
@@ -7,5 +8,6 @@ app = Quart(__name__)
 @app.route("/payload", methods=["POST"])
 async def inspect_push():
     push = await request.get_json()
+    time.sleep(3)
     app.logger.debug(json.dumps(push, indent=2))
     return "", 204
